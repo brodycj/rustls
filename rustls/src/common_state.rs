1c88c57078cd8633adbd08ebe8ab5a78e647e998
@brodycj - C. Jonathan Brody <cj@brodycj.com>
2024-08-21 09:51:41 -0400

    xxx-xxx-trait:State

--- rustls/src/common_state.rs
+++ rustls/src/common_state.rs
@@ -814,7 +814,7 @@ impl IoState {
     }
 }
 
-pub(crate) trait State<Data>: Send + Sync {
+pub(crate) trait State<Data> {
     fn handle<'m>(
         self: Box<Self>,
         cx: &mut Context<'_, Data>,
