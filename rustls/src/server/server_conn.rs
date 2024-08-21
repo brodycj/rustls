9bdbd9547bb87f741a296f3a88cae0247db10d1f
@brodycj - C. Jonathan Brody <cj@brodycj.com>
2024-08-21 10:10:33 -0400

    xxx:ResolvesServerCert

--- rustls/src/server/server_conn.rs
+++ rustls/src/server/server_conn.rs
@@ -114,7 +114,7 @@ pub trait ProducesTickets: Debug + Send + Sync {
 /// For applications that use async I/O and need to do I/O to choose
 /// a certificate (for instance, fetching a certificate from a data store),
 /// the [`Acceptor`] interface is more suitable.
-pub trait ResolvesServerCert: Debug + Send + Sync {
+pub trait ResolvesServerCert: Debug {
     /// Choose a certificate chain and matching key given simplified
     /// ClientHello information.
     ///
