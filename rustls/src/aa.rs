8583163efd70392bf98de79e99e270bfb4d9353d
@brodycj - C. Jonathan Brody <cj@brodycj.com>
2024-08-21 10:37:47 -0400

    no-more-separate-aa-module-for-now

--- rustls/src/aa.rs
+++
@@ -1,5 +0,0 @@
-#[cfg(feature = "arcshare")]
-pub(crate) use alloc::sync::Arc;
-
-#[cfg(not(feature = "arcshare"))]
-pub(crate) use alloc::rc::Rc as Arc;
--- rustls/src/lib.rs
+++ rustls/src/lib.rs
@@ -390,7 +390,14 @@ mod log {
 #[macro_use]
 mod test_macros;
 
-mod aa;
+// XXX TODO RECONSIDER MODULE NAMING & MOVE TO SEPARATE SOURCE FILE
+mod aa {
+    #[cfg(feature = "arcshare")]
+    pub(crate) use alloc::sync::Arc;
+
+    #[cfg(not(feature = "arcshare"))]
+    pub(crate) use alloc::rc::Rc as Arc;
+}
 
 #[macro_use]
 mod msgs;
