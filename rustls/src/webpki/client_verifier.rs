7d4af1c960abf4799a5cce153a8874b8145aae18
CJB <cjb@vultr.guest>
2024-08-22 23:20:44 +0000

    fixup-xxx-doctest-xxx

--- rustls/src/webpki/client_verifier.rs
+++ rustls/src/webpki/client_verifier.rs
@@ -199,7 +199,10 @@ impl ClientCertVerifierBuilder {
 ///
 /// To require all clients present a client certificate issued by a trusted CA:
 /// ```no_run
-/// # #[cfg(any(feature = "ring", feature = "aws_lc_rs"))] {
+/// # #[cfg(all(
+/// #   feature = "defaultproviderenabled",
+/// #   any(feature = "ring", feature = "aws_lc_rs")
+/// # ))] {
 /// # use rustls::RootCertStore;
 /// # use rustls::server::WebPkiClientVerifier;
 /// # let roots = RootCertStore::empty();
@@ -212,7 +215,10 @@ impl ClientCertVerifierBuilder {
 /// Or, to allow clients presenting a client certificate authenticated by a trusted CA, or
 /// anonymous clients that present no client certificate:
 /// ```no_run
-/// # #[cfg(any(feature = "ring", feature = "aws_lc_rs"))] {
+/// # #[cfg(all(
+/// #   feature = "defaultproviderenabled",
+/// #   any(feature = "ring", feature = "aws_lc_rs")
+/// # ))] {
 /// # use rustls::RootCertStore;
 /// # use rustls::server::WebPkiClientVerifier;
 /// # let roots = RootCertStore::empty();
@@ -234,7 +240,10 @@ impl ClientCertVerifierBuilder {
 /// You can also configure the client verifier to check for certificate revocation with
 /// client certificate revocation lists (CRLs):
 /// ```no_run
-/// # #[cfg(any(feature = "ring", feature = "aws_lc_rs"))] {
+/// # #[cfg(all(
+/// #   feature = "defaultproviderenabled",
+/// #   any(feature = "ring", feature = "aws_lc_rs")
+/// # ))] {
 /// # use rustls::RootCertStore;
 /// # use rustls::server::{WebPkiClientVerifier};
 /// # let roots = RootCertStore::empty();
