70db9e37569821fb9f557dcace03ab38c6240ed2
@brodycj - C. Jonathan Brody <cj@brodycj.com>
2024-08-27 18:39:38 -0400

    simplify-xx-with-unwrap-or-else-xxx

--- rustls/src/crypto/mod.rs
+++ rustls/src/crypto/mod.rs
@@ -246,21 +246,13 @@ impl CryptoProvider {
     /// - panics about the need to call [`CryptoProvider::install_default()`]
     // #[cfg(feature = "defaultproviderenabled")] // XXX TODO COMPLETELY REMOVE THIS FEATURE CONFIG
     pub(crate) fn get_default_or_install_from_crate_features() -> Arc<Self> {
-        match static_default::get_default() {
-            Some(provider) => provider,
-            None => {
-                let _ = Self::from_crate_features()
-                    .expect("no process-level CryptoProvider available -- call CryptoProvider::install_default() before this point")
-                    .install_default();
-                // Ignore the error resulting from us losing a race, and accept the outcome.
-                static_default::get_default().unwrap()
-            }
-        }
-        // let provider = Self::from_crate_features()
-        //     .expect("no process-level CryptoProvider available -- call CryptoProvider::install_default() before this point");
-        // // Ignore the error resulting from us losing a race, and accept the outcome.
-        // let _ = provider.install_default();
-        // Self::get_default().unwrap()
+        static_default::get_default().unwrap_or_else(|| {
+            let _ = Self::from_crate_features()
+                .expect("no process-level CryptoProvider available -- call CryptoProvider::install_default() before this point")
+                .install_default();
+            // Ignore the error resulting from us losing a race, and accept the outcome.
+            static_default::get_default().unwrap()
+        })
     }
 
     /// Returns a provider named unambiguously by rustls crate features.
