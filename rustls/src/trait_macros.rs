785ff30454c8eaa8993a32b20f3d178c01fbe035
@brodycj - C. Jonathan Brody <cj@brodycj.com>
2024-09-04 23:09:36 -0400

    revert-xxx:rustls/src/crypto/signer.rs,etc

--- rustls/src/trait_macros.rs
+++ rustls/src/trait_macros.rs
@@ -1,27 +1,9 @@
-/// pub trait - version with doc - version that includes Send & Sync - supports use with alloc::sync::Arc
-#[cfg(not(feature = "withrcalias"))]
-macro_rules! pub_api_trait_with_doc {
-    ($doc_text: literal, $name:ident, $body:tt) => {
-        #[doc = $doc_text]
-        pub trait $name: core::fmt::Debug + Send + Sync $body
-    }
-}
-
 /////// XXX TODO REPLACE ALL USE OF THIS MACRO WITH pub_api_trait_with_doc! (with doc fixed) & REMOVE THIS MACRO
 /// pub trait - version with no doc - version that includes Send & Sync - supports use with alloc::sync::Arc
 #[cfg(not(feature = "withrcalias"))]
 macro_rules! pub_api_trait {
     ($name:ident, $body:tt) => {
         pub trait $name: core::fmt::Debug + Send + Sync $body
-    }
-}
-
-/// pub trait - version with doc - version with no Send / Sync - supports use with alloc::rc::Rc
-#[cfg(feature = "withrcalias")]
-macro_rules! pub_api_trait_with_doc {
-    ($doc_text: literal, $name:ident, $body:tt) => {
-        #[doc = $doc_text]
-        pub trait $name: core::fmt::Debug $body
     }
 }
 
