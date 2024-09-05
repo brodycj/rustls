029a26db7a196bdbbbe061c4ca8be8402c12037b
@brodycj - C. Jonathan Brody <cj@brodycj.com>
2024-09-04 23:11:25 -0400

    revert-xxx:rustls/src/common_state.rs

--- rustls/src/trait_macros.rs
+++ rustls/src/trait_macros.rs
@@ -15,21 +15,3 @@ macro_rules! pub_api_trait {
         pub trait $name: core::fmt::Debug $body
     }
 }
-
-/// internal pub(crate) trait that includes Send & Sync - supports use with alloc::sync::Arc
-#[cfg(not(feature = "withrcalias"))]
-macro_rules! internal_generic_state_trait {
-    // XXX QUICK HACKY MACRO API WITH SEPARATE NAME & GENERIC TYPE PARAMETERS
-    ($name:ident, $generic_type_parameter:ident, $body:tt) => {
-        pub(crate) trait $name<$generic_type_parameter>: Send + Sync $body
-    }
-}
-
-/// internal pub(crate) trait with no Send / Sync - supports use with alloc::rc::Rc
-#[cfg(feature = "withrcalias")]
-macro_rules! internal_generic_state_trait {
-    // XXX QUICK HACKY MACRO API WITH SEPARATE NAME & GENERIC TYPE PARAMETERS
-    ($name:ident, $generic_type_parameter:ident, $body:tt) => {
-        pub(crate) trait $name<$generic_type_parameter> $body
-    }
-}
