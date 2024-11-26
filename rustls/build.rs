c6eb6d170ddc7b880279da2dc93c1e99e7f4c82e
@brodycj - C. Jonathan Brody <cj@brodycj.com>
2024-11-26 13:47:03 -0500

    update-build-fn-naming-and-fixup-xxx-todo-update-comment-spelling

--- rustls/build.rs
+++ rustls/build.rs
@@ -1,23 +1,23 @@
-/// XXX TODO UDPATE THIS:
+/// XXX TODO UPDATE THIS:
 /// This build script allows us to enable the `read_buf` language feature only
 /// for Rust Nightly.
 ///
 /// See the comment in lib.rs to understand why we need this.
 
 #[rustversion::nightly]
-fn setup_unstable_clippy() {
+fn setup_cfg_unstable_clippy() {
     println!("cargo:rustc-check-cfg=cfg(unstable_clippy)");
     println!("cargo:rustc-cfg=unstable_clippy");
 }
 
 #[rustversion::not(nightly)]
-fn setup_unstable_clippy() {
+fn setup_cfg_unstable_clippy() {
     // XXX TODO REPEATED STATEMENT(S) HERE:
     println!("cargo:rustc-check-cfg=cfg(unstable_clippy)");
 }
 
 #[cfg_attr(feature = "read_buf", rustversion::not(nightly))]
-fn setup_read_buf() {
+fn setup_cfg_read_buf() {
     // XXX TODO REPEATED STATEMENT(S) HERE:
     println!("cargo:rustc-check-cfg=cfg(bench)");
     println!("cargo:rustc-check-cfg=cfg(read_buf)");
@@ -25,14 +25,14 @@ fn setup_read_buf() {
 
 #[cfg(feature = "read_buf")]
 #[rustversion::nightly]
-fn setup_read_buf() {
+fn setup_cfg_read_buf() {
     println!("cargo:rustc-check-cfg=cfg(bench)");
     println!("cargo:rustc-check-cfg=cfg(read_buf)");
     println!("cargo:rustc-cfg=read_buf");
 }
 
 fn main() {
-    setup_unstable_clippy();
+    setup_cfg_unstable_clippy();
 
-    setup_read_buf();
+    setup_cfg_read_buf();
 }
