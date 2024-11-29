f0a0b4d89348f39d862768a37aafcda9cec0ed0b
@brodycj - C. Jonathan Brody <cj@brodycj.com>
2024-11-28 19:45:36 -0500

    update provider example to support optional critical-section feature

--- provider-example/src/lib.rs
+++ provider-example/src/lib.rs
@@ -4,9 +4,9 @@ extern crate alloc;
 #[cfg(feature = "std")]
 extern crate std;
 
-use alloc::sync::Arc;
-
 use rustls::crypto::CryptoProvider;
+// XXX TODO USAGE OF INTERNAL, UNDOCUMENTED FEATURE IN AN EXAMPLE
+use rustls::internal::alias::Arc;
 use rustls::pki_types::PrivateKeyDer;
 
 mod aead;
